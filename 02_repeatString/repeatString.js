dbacefc73e725d539788719d27c55da3443e5852
sk8rh8r7717 <64029051+sk8rh8r7717@users.noreply.github.com>
2025-02-25 21:37:55 +0500

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    newString = ""
+    if (repeats < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < repeats; i++) {
+        newString = newString + string;
+    }
+    return newString
 };
 
 // Do not edit below this line
