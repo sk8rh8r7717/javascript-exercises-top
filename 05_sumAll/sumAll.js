05b5d0dc6911a1f054039a33841d83839ff771a9
sk8rh8r7717 <64029051+sk8rh8r7717@users.noreply.github.com>
2025-03-02 12:34:57 +0500

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || a%1 != 0 || b%1 != 0 || typeof(a) != "number" || typeof(b) != "number"){
+        return "ERROR"
+    }
+    sum = 0
+    if (a > b) {
+        let temp = a
+        a = b
+        b = temp
+    }
+    for (i = a; i <= b; i++) {
+        sum = i + sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
