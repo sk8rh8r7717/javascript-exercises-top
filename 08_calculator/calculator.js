4cec4f6afeef479cbe101d325d2891f87b69d0ab
sk8rh8r7717 <64029051+sk8rh8r7717@users.noreply.github.com>
2025-03-10 15:31:35 +0500

    Solved 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  return numArray.reduce(add, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let total = 1
+  for (i=0; i<numArray.length; i++) {
+    total = total * numArray[i]
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return (num1 ** num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1
+  for (let i=1; i<=num; i++) {
+    total = total * i
+  }
+  return total
 };
 
+console.log(multiply(2, 3));
 // Do not edit below this line
 module.exports = {
   add,
