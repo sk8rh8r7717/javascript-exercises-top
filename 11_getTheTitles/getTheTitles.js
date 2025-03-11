462bb931f198cf2c59ff954ce7adc222cfa5974e
sk8rh8r7717 <64029051+sk8rh8r7717@users.noreply.github.com>
2025-03-11 06:07:06 +0500

    Solved 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArr) {
+    function getTitleFromElement (Book) {
+        return Book.title
+    }
+    return bookArr.map(getTitleFromElement)
 };
 
 // Do not edit below this line
