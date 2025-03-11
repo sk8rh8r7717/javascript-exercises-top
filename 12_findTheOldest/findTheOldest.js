f0289e2ce3c47753c9b00a5bc7761b9ba2476647
sk8rh8r7717 <64029051+sk8rh8r7717@users.noreply.github.com>
2025-03-11 15:49:48 +0500

    Solved 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    let max = {
+        name: "",
+        yearOfBirth: 0,
+        yearOfDeath: 0
+    }
 
+    for (let i = 0; i < peopleArr.length; i++) {
+        if (peopleArr[i].yearOfDeath == undefined) {
+            peopleArr[i]["yearOfDeath"] = 2025
+
+        }
+        if ((peopleArr[i].yearOfDeath - peopleArr[i].yearOfBirth) > (max.yearOfDeath - max.yearOfBirth)) {
+            max = peopleArr[i]
+        }
+    }
+
+    return max
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+console.log(findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]))
