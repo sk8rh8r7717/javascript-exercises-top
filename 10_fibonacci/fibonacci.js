00b46f70e533cafc865f97ee2554e1b2ee5910d3
sk8rh8r7717 <64029051+sk8rh8r7717@users.noreply.github.com>
2025-03-11 06:02:17 +0500

    Solved 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(memberCount) {
+    let prevNum = 1;
+    let currentNum = 1;
+    let temp;
 
+    if (memberCount == 0) {
+        return 0
+    }
+    else if (memberCount < 0) {
+        return "OOPS"
+    }
+    for (let i = 0; i < memberCount - 2; i++) {
+        temp = currentNum;
+        currentNum = prevNum + currentNum;
+        prevNum = temp;
+    }
+
+    return currentNum
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+console.log(fibonacci("1"))
